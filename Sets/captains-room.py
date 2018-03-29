8c3999faad71581d6978125834d9b73bd30e3780
Karina Bekbayeva <35498147+karinabk@users.noreply.github.com>
2018-03-29 14:45:35 +0600

    Create captains-room.py

---
+++ Sets/captains-room.py
@@ -0,0 +1,18 @@
+
+
+
+if __name__=='__main__':
+    n=int(input()) # number of persons per family
+    list1=list(map(int,input().split())) # all people 
+    set1=set()
+    set1.update(list1)
+    for i in list1:
+        if len(set1)==1:
+            break
+        set1.remove(i)
+    listResult = list(set1)
+    print(listResult[0])
+    
+    
+    
+  #  does not work yet
