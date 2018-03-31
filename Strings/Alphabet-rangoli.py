fa587364a0008b1ac9b7d28ad0241aec0d250217
Karina Bekbayeva <35498147+karinabk@users.noreply.github.com>
2018-04-01 00:58:31 +0600

    Update Alphabet-rangoli.py

--- Strings/Alphabet-rangoli.py
+++ Strings/Alphabet-rangoli.py
@@ -22,94 +22,47 @@ oNOT WORKING YET#You are given an integer,N.
 #----e-d-c-d-e----
 #------e-d-e------
 #--------e--------
-
 def print_rangoli(size):
-    
-    charinc=0
-    chardec=0
-    char_='a'
-    string = ""
-    first=chr(ord(char_)+size-1)
-    for line in range(size):
-        chardec=0
-        for i in range(line+1):
-            # print(string)
-            string+=chr(ord(char_)+size-1-chardec)
-            string+="-"
-            if (ord(char_)+size-1-chardec)-ord(char_)==line+1:
-                print("FREE")
-                break
-            chardec+=1
-        # for k in range(line):
-        #     string+=chr(ord(char_)+size-1+charinc)
-        #     string+="-"
-        #     charinc+=1
-        
-        print(string.center(4*size-3,"-"))
-    
-    
-    
-    
-    
-#     line=0
-#     chardec=0
-#     levaya=1
-#     for i in range(size):
-#         for k in range((size+size-1+(size+size-2))//2-line):
-#             print("-", end="")
-#         for l in range(levaya):
-#             print(chr(ord(char_)+size-1-chardec), end="")
-            
-#             print(chr(ord(char_)+size-1-chardec), end="")
-#             print("-")
-#         for z in range(levaya-1):
-            
-#         for m in range((size+size-1+(size+size-2))//2-line):
-#             print("-")
-#         print("")
-#         levaya+=1
-#         line +=2
-#         chardec+=1
-    
- #  TRIED FOR PLENTY oF TIMES 
- #IS NOT WORKIN YET
-    def print_rangoli(size):
     a='a'
-    charinc=0
-    chardec=0
-    string = ""
-    for line in range(size):
-        number1 =0
-        print("first loop")
+    for line in range(size): #loop for line
         string1=""
-        for i in range(size):
+        chardec=0
+        for i in range(size): # putting letters in decreasing order
             string1+=chr(ord(a)+size-1+chardec)
             chardec-=1
-            if i==number1:
-                number1+=1
+            if i==line:
                 break
-            
-            if chr(ord(a)+size-1+chardec)=='a':
-                break
-        print(string1)    
-        print("end first")
-        print("second loop")
+        if line>=1: # increasing order starts from second line
+            charinc=1
+            for k in range(size-1): #putting letters in increasing order 
+                string1+=chr(ord(a)+size+chardec+charinc)
+                charinc+=1
+                if k==line-1:
+                    break
+        string1=list(string1)
+        string1=str("-".join(string1)) # putting - between letters
+        print(string1.center(4*size-3,"-"))
+    for line2 in range(size-1): # second half of the rangoli
         string2=""
-        number2=0
-        for k in range(size):
-            string2+=chr(ord(a)+size-2+charinc)
-            charinc+=1
-            if k==number2+1:
-                number2+=1
+        chardec=0
+        for i in range(size-1): # putting letters in decreasing order
+            string2+=chr(ord(a)+size-1-chardec)
+            chardec+=1
+            if i ==size-line2-2:
                 break
-                
-            
-            if chr(ord(a)+size-1-chardec)==chr(ord(a)+size-2+charinc):
-                break
-        print(string2)
-        print("end second")
-        print("RESULT")
-        print(string)
+        charinc=1
+        if line2!=size-2: # excluding last line
+            for j in range(size-2): # putting letters in increasing order
+                string2+=chr(ord(a)+size-chardec+charinc)
+                if j==size-3-line2:
+                    break
+                charinc+=1
+                string1=list(string2)
+        string2=str("-".join(string2))
+        print(string2.center(4*size-3,"-"))
         
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
         
-       # TRIED TO IMPLEMENT IT IN ANOTHER WAY
+        #IT WORKS NOW
