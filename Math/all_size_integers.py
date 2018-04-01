deb353b07d47d34ab4662526128f0d85c86e3aab
Karina Bekbayeva <35498147+karinabk@users.noreply.github.com>
2018-04-01 23:31:53 +0600

    Create all_size_integers.py

---
+++ Math/all_size_integers.py
@@ -0,0 +1,11 @@
+#Task
+#Read four numbers, a, b, c, and d, and print the result of 
+#a^b+c^d. 
+
+
+if __name__ =='__main__':
+    num1=int(input())
+    pow1=int(input())
+    num2=int(input())
+    pow2=int(input())
+    print(pow(num1,pow1)+pow(num2,pow2))
