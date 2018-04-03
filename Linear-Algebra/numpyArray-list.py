b836664b75076c9bd1f4fe15f768ca4b866104b6
Karina Bekbayeva <35498147+karinabk@users.noreply.github.com>
2018-04-03 16:41:38 +0600

    Create numpyArray-list.py

---
+++ Linear-Algebra/numpyArray-list.py
@@ -0,0 +1,14 @@
+
+#Challenge
+
+#Write code for two vectors with five values of your choice.
+#The first should be written as a regular one-dimensional list. 
+#The other should be be written with numpy.
+
+
+import numpy as np
+vector1=[2,1,3,4,-9]
+magnitude1 = np.linalg.norm(vector1)
+vector2 = np.array([2,1,3,4,-9])
+magnitude2=np.linalg.norm(vector2)
+magnitude2
