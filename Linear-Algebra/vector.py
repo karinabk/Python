b200b93447835b2ba16711b2ca57c52e5d713767
Karina Bekbayeva <35498147+karinabk@users.noreply.github.com>
2018-04-03 16:30:15 +0600

    Update vector.py

--- Linear-Algebra/vector.py
+++ Linear-Algebra/vector.py
@@ -8,3 +8,10 @@ coords2 = np.array([1,1])
 magnitude = np.linalg.norm(coords1-coords2)
 coords3=coords1-coords2
 direction = np.arctan(coords3[1]/coords3[0])
+#without numpy
+coords1 = [5,3]
+coords2 = [1,1]
+coords3=[]
+coords3.append(coords1[0]-coords2[0])
+coords3.append(coords1[1]-coords2[1])
+magnitude=(coords3[1]**2+coords3[0]**2)**(1/2)
