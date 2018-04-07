387510904d2119a4309d16e7f38cac296c368bee
Karina Bekbayeva <35498147+karinabk@users.noreply.github.com>
2018-04-07 15:55:06 +0600

    Update permutatuins.py

--- Itertools/permutatuins.py
+++ Itertools/permutatuins.py
@@ -1,21 +1,17 @@
 #Task 
-#You are given a string S. 
+#You are given a string S. and number k
 #Your task is to print all possible permutations of size k of the string in lexicographic sorted order.
-
 
 
 from itertools import permutations
 
-string = list(map(str,input().split()))
-n = int(string[1])
-
-result = list(permutations(string[0],2))
-result = list(map(str,result))
-out=[]
+list1 = list(map(str,input().split()))         #store input as a list of strings
+num = int(list1[1])                            # takes number of letters in one group
+result = list(permutations(list1[0],num))      #creates list of permutations of type tuples
+result = list(map(list,result))                # converts tuples to list
+l=[]                                           # list for storing one permutation 
 for i in result:
-    i=list(map(str,i))
-    out.append(i[2])
-    out.append(i[7])
-    print(out)
-    
-    #DOES NOT WORK YET
+    i="".join(i)                               #converts from list to string
+    l.append(i)
+l.sort()                                       #sorting in order 
+print("\n".join(l))                            #printing on new line
