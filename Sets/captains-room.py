ebfcfa19ea25cfbbea51073ce6e080121676dbcd
Karina Bekbayeva <35498147+karinabk@users.noreply.github.com>
2018-04-13 21:53:57 +0600

    Update captains-room.py

--- Sets/captains-room.py
+++ Sets/captains-room.py
@@ -1,3 +1,17 @@
+'''Task
+Mr. Anant Asankhya is the manager at the INFINITE hotel. The hotel has an infinite amount of rooms. 
+One fine day, a finite number of tourists come to stay at the hotel. 
+The tourists consist of:
+→ A Captain.
+→ An unknown group of families consisting of K members per group where K≠1.
+The Captain was given a separate room, and the rest were given one room per group.
+Mr. Anant has an unordered list of randomly arranged room entries. The list consists of the room numbers for all of the tourists. 
+The room numbers will appear K times per group except for the Captain's room. 
+Mr. Anant needs you to help him find the Captain's room number. 
+The total number of tourists or the total number of groups of families is not known to you. 
+You only know the value of K and the room number list.'''
+
+
 
 
 
@@ -16,3 +30,6 @@ if __name__=='__main__':
     
     
   #  does not work yet
+
+
+    
