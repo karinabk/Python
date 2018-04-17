062c1a1d1b0a3e6b07e6b18f5a3a6b7bafcc9f77
Karina Bekbayeva <35498147+karinabk@users.noreply.github.com>
2018-04-17 20:39:07 +0600

    Create input.py

---
+++ Built-ins/input.py
@@ -0,0 +1,13 @@
+'''Task 
+You are given a polynomial P
+of a single indeterminate (or variable), x . 
+You are also given the values of x and k . Your task is to verify if 
+P(x)=k.'''
+
+
+
+
+
+input_ = input().split()
+x = int(input_[0])
+print(eval(input()) == int(input_[1]))
