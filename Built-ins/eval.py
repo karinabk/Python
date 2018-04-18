5aced889b35ec7539b63f9bba169e1e210c02a41
Karina Bekbayeva <35498147+karinabk@users.noreply.github.com>
2018-04-18 09:57:16 +0600

    Create eval.py

---
+++ Built-ins/eval.py
@@ -0,0 +1,10 @@
+'''
+Task 
+You are given an expression in a line. 
+Read that line as a string variable, such as var, and print the result using eval(var). '''
+
+#Can you solve it using one line?
+
+#Solution
+
+eval(input())
